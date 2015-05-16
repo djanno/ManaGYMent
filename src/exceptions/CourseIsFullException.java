31e7cba6dc160858f8805d7d9f26fdf4d0bee11a
moneletizi <moneletizi@localhost>
2015-05-16 20:02:00 +0200

    modificate tutte le eccezioni

--- src/exceptions/CourseIsFullException.java
+++ src/exceptions/CourseIsFullException.java
@@ -3,8 +3,9 @@ package exceptions;
 public class CourseIsFullException extends Exception {
 
 	private static final long serialVersionUID = 2514525621771861881L;
-
-	public CourseIsFullException(final String message) {
-		super(message);
+	private static final String MESSAGE = "Il corso ha già raggiunto il massimo degli iscritti";
+	
+	public CourseIsFullException() {
+		super(MESSAGE);
 	}
 }
