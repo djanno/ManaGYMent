dd211e21bae7808cb491a54789e35bb9782266e2
Borfi <Borfi@localhost>
2015-05-24 18:33:41 +0200

    modificate parametri cmdSave

--- src/controller/panels/members/IEmployeeEditController.java
+++ src/controller/panels/members/IEmployeeEditController.java
@@ -7,6 +7,6 @@ import javax.swing.DefaultListModel;
 import view.panels.members.IFormField;
 
 public interface IEmployeeEditController {
-	void cmdSave(final Map<IFormField,String> mapToPass, String salario, DefaultListModel<String> list);
+	void cmdSave(final Map<IFormField,String> mapToPass, String salario);
 	void loadData(); //int index
 }
